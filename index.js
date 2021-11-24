86c593035a2f7d7875e00547d7f8b02a29629fc4
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-24 09:36:39 -0600

    function display colors

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+/* 
+    Use fetch() to load 1, then 5, then 25 colors from the HexBot
+
+    Use FlexBox to create a Responsive 5x5 Grid displaying the colors    
+*/
+
+function displayColors(colors) {
+    console.log(colors)
+    
+
+}
+
+
 async function getColors() {
     let response = await fetch("https://apis.scrimba.com/hexcolors?count=5")
     let data = await response.json()
+    let colors = data.colors
     
-    console.log(data)
+    displayColors(colors)
 }
 
 getColors()
